460bc5049b923a1d7d9d84a8895f45be434ba4b0
FosterSOAsare <fostersoasare@gmail.com>
2023-12-29 19:32:54 +0000

    refactors

--- src/components/pages/Homepage/MainContent.tsx
+++ src/components/pages/Homepage/MainContent.tsx
@@ -38,7 +38,7 @@ const MainContent = ({ setSelected }: { setSelected: React.Dispatch<React.SetSta
 
 	return (
 		<article className="w-full lg:w-2/3  h-auto min-h-screen p-4 px-6 backdrop-blur">
-			<section className="lg:ml-4 w-full lg:w-[90%] pt-0 md:pt-6 mt-6 lg:pt-12 lg:mt-12" id="about" ref={aboutRef} data-aos="fade-left">
+			<section className="lg:ml-4 w-full lg:w-[90%] pt-0 md:pt-6 mt-6 lg:pt-10 lg:mt-12" id="about" ref={aboutRef} data-aos="fade-left">
 				<p className="w-full lg:leading-[30px]  mb-4 ">
 					In 2020, I discovered my passion for web and mobile development during my Senior High School years. Self-driven learning, tutorials, and practice led me to secure my first contract
 					as a PHP developer at UNIFIN. Subsequent experiences with startups and senior developers further enriched my skills, resulting in the creation of high-performance projects.
--- src/components/pages/Homepage/UserInfo.tsx
+++ src/components/pages/Homepage/UserInfo.tsx
@@ -13,7 +13,7 @@ const UserInfo = ({ selected, setSelected }: { selected: number; setSelected: Re
 		});
 	}, []);
 	return (
-		<article className="py-12 lg:py-24 w-full lg:w-1/2  px-4 lg:sticky top-0 left-0 h-auto lg:h-screen flex flex-col justify-start gap-8">
+		<article className="py-12 lg:py-22 w-full lg:w-1/2  px-4 lg:sticky top-0 left-0 h-auto lg:h-screen flex flex-col justify-start gap-8">
 			<div className="">
 				<div className="w-16 h-16 overflow-hidden border-[3px] bg-slate-100 mb-4 relative rounded-full" data-aos="fade-down">
 					<Image src={ProfileImage} alt="Asare Foster" fill></Image>
